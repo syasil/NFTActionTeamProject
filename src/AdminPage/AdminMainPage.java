f01858a8cde7339e5895eaee54da15eebddc236b
harteh <harteh31@gmail.com>
2022-06-24 18:15:03 +0900

    커밋 테스트

--- src/AdminPage/AdminMainPage.java
+++ src/AdminPage/AdminMainPage.java
@@ -6,7 +6,8 @@ package AdminPage;
  * 	1. 관리자로그인, 로그아웃 버튼 생략 (회원 로그인 작업과 동일)
  * 		- 관리자 id로 로그인 시 접근 가능
  * 		- 로그아웃 시 프로젝트 메인 화면으로 이동됨
- * 	2. 등록된 NFT 목록 출력
+ * 	2. 등록된 NFT(상품) 목록 출력
+ * 		- no, 상품 이름
  * 
  * 
  * @author harteh
@@ -16,5 +17,7 @@ public class AdminMainPage {
 	public static void main(String[] args) {
 		// 관리자 로그인 후 보여지는 화면
 		System.out.println("첫 push 테스트");
+		System.out.println("push 테스트");
+		System.out.println("push 테스트");
 	}
 }
