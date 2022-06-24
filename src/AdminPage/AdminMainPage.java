a7d78a279a04de783abbc4a4c3e5d2c7d9e009f2
harteh <harteh31@gmail.com>
2022-06-24 17:10:26 +0900

    new Branch-kbm31

---
+++ src/AdminPage/AdminMainPage.java
@@ -0,0 +1,20 @@
+package AdminPage;
+
+
+/**
+ * 관리자 로그인 후 보여지는 화면
+ * 	1. 관리자로그인, 로그아웃 버튼 생략 (회원 로그인 작업과 동일)
+ * 		- 관리자 id로 로그인 시 접근 가능
+ * 		- 로그아웃 시 프로젝트 메인 화면으로 이동됨
+ * 	2. 등록된 NFT 목록 출력
+ * 
+ * 
+ * @author harteh
+ *
+ */
+public class AdminMainPage {
+	public static void main(String[] args) {
+		// 관리자 로그인 후 보여지는 화면
+		System.out.println("첫 push 테스트");
+	}
+}
