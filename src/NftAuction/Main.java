22fad4dd181fe32922d4a25bc05760af04aadee1
syasil <syasil2402@gmail.com>
2022-06-24 17:34:14 +0900

    브렌치 병합 준비

--- src/NftAuction/Main.java
+++ src/NftAuction/Main.java
@@ -46,14 +46,15 @@ public class Main {
 		
 		
 		mainFrame = new JFrame();
-		mainFrame.setBounds(100, 100, 900, 600);
+		mainFrame.setBounds(100, 100, 600, 900);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.getContentPane().setLayout(null);
 		
 		
 		
 		
-		FacadeProductRegisterPage facadeprodictRegisterPage = new FacadeProductRegisterPage(mainFrame);
+		//FacadeProductRegisterPage facadeprodictRegisterPage = new FacadeProductRegisterPage(mainFrame);
+		//상품 등록 페이지 생성 객체 나중에 버튼 연결해야함
 		
 		//FacadeJoinPage GeneralUserJoinPage = new FacadeJoinPage(1, mainFrame);
 		//1 == 일반회원 ,2 == 관리자회원
