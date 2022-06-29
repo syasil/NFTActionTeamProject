c31b117131a515cfa1122f5b3359fe0efdae9ad4
soldesk <soldesk@DESKTOP-G831PP6>
2022-06-29 18:58:07 +0900

    프로시저로 작성

--- src/db/SampleProc.java
+++ src/db/SampleProc.java
@@ -12,33 +12,23 @@ public class SampleProc {
 	public static void main(String[] args) throws SQLException {
 		Connection conn = null; // DB와 연결하는 인터페이스
 		PreparedStatement psmt = null; // sql 문 객체
+		CallableStatement cs;
 		ResultSet rs = null; // sql에 대한 반환(쿼리 실행에 대한 결과값 저장
-		
-		
-
-		
 		
 		
 		
 		
 		try {
-			String proc = "proc_emp_get_all";
-
 			conn = DB.get(); // DB연결
 
 			// 프로시저 호출
-			 CallableStatement cs = conn.prepareCall("begin proc_emp_get_all(?); end;");
-
-			// 입력 파라메터
-			//cs.setInt(1, 1000);
-			
-			// 출력 파라메터
+			cs = conn.prepareCall("begin proc_emp_get_all(?); end;");
 			cs.registerOutParameter(1, oracle.jdbc.OracleTypes.CURSOR);
-			 
+			//cs.setInt(2, 1000);
+
 			// 실행
 			cs.execute();
 			rs = (ResultSet)cs.getObject(1);
-			
 			
 			while(rs.next()) {
 				int empno = rs.getInt(1);
