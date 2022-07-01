cc175754aefdaf4cb45a5894ed418144740ff8b0
harteh <harteh31@gmail.com>
2022-07-01 11:34:59 +0900

    회원아이디로 경매내역 가져오기 프로시저 추가중
    매개변수 유형 충돌 오류생김

--- src/AdminPage/AdminMainPage.java
+++ src/AdminPage/AdminMainPage.java
@@ -2,6 +2,7 @@ package AdminPage;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -11,12 +12,14 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.geom.Line2D;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -28,6 +31,7 @@ import swing.ALabel;
 import swing.APanel;
 import swing.AScrollPane;
 import swing.ATable;
+import swing.ATextField;
 
 public class AdminMainPage extends JFrame {
 
@@ -36,7 +40,7 @@ public class AdminMainPage extends JFrame {
 	PreparedStatement psmt = null;
 	ResultSet rs = null;
 	
-	// 데이터 가져오기 테스트
+	// 데이터 가져오기
 	GetData getData = new GetData();
 
 	// 관리자 로그인 후 보여지는 화면
@@ -58,57 +62,68 @@ public class AdminMainPage extends JFrame {
 		String todayAucCnt = getData.todayAuc;
 
 		// 1.오늘 진행된 경매 건수 데이터
-		ALabel jlAucCnt = new ALabel("진행경매 : " + todayAucCnt + "건");
-		jlAucCnt.setBounds(200, 150, 300, 20);
+		ALabel jlAucCnt = new ALabel("오늘 진행된 경매 : " + todayAucCnt + "건");
+		jlAucCnt.setBounds(50, 130, 200, 20);
+		jlAucCnt.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		c.add(jlAucCnt);
-		// 2.오늘 거래된 경매 금액
+		// 경매 금액 표시
 		ALabel jlAucPrice = new ALabel("거래금액 : " + todayAucCnt + "원");
-		jlAucPrice.setBounds(400, 150, 300, 20);
+		jlAucPrice.setBounds(300, 130, 200, 20);
 		c.add(jlAucPrice);
+		
+		// 2.오늘 거래된 경매 금액
+		ALabel jlUserAuc = new ALabel("경매내역검색 : ");
+		ATextField jtUserAuc = new ATextField();
+		AButton jbUserAuc = new AButton("검색");
+		
+		jlUserAuc.setBounds(50, 160, 150, 20);
+		jtUserAuc.setBounds(200, 160, 150, 20);
+		jbUserAuc.setBounds(420, 160, 100, 25);
+		jbUserAuc.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		c.add(jlUserAuc);
+		c.add(jtUserAuc);
+		c.add(jbUserAuc);
 		// 데이터 가져오기 끝
 
 		// 화면 중간 데이터 보여주는 영역
 		// 1.경매건수 - 경매내역 리스트
 		JPanel jPAucCnt = new JPanel();
 		jPAucCnt.setBackground(Color.decode("#3ace89"));
-		jPAucCnt.setBounds(50, 190, 470, 320);
+		jPAucCnt.setBounds(50, 200, 470, 320);
 		jPAucCnt.setVisible(true);
 		c.add(jPAucCnt);
-		// 2.경매금액 - 거래회원목록 리스트
-		JPanel jPAucPrice = new JPanel();
-		jPAucPrice.setBackground(Color.decode("#f45728"));
-		jPAucPrice.setBounds(50, 190, 470, 320);
-		jPAucPrice.setVisible(false);
-		c.add(jPAucPrice);
 
-		// 테이블 생성
-		//1.경매건수에 보여주는 리스트 - 
-		DefaultTableModel modelAuc = new DefaultTableModel(new String[] { "경매번호", "낙찰그림번호", "낙찰자번호" }, 0);
+		// 테이블 생성 1
+		//1.경매건수 - 오늘 진행된 경매 리스트 출력 
+		DefaultTableModel modelAuc = new DefaultTableModel(new String[] { "경매번호", "그림번호", "그림명", "낙찰자id" }, 0);
 		ATable aucListTable = new ATable(modelAuc);
-
+		
 		// 각 셀 사이즈 설정
-		aucListTable.getColumn("경매번호").setPreferredWidth(20);
-		aucListTable.getColumn("낙찰그림번호").setPreferredWidth(150);
-		aucListTable.getColumn("낙찰자번호").setPreferredWidth(200);
+		aucListTable.getColumn("경매번호").setPreferredWidth(40);
+		aucListTable.getColumn("그림번호").setPreferredWidth(40);
+		aucListTable.getColumn("그림명").setPreferredWidth(200);
+		aucListTable.getColumn("낙찰자id").setPreferredWidth(190);
 
 		AScrollPane scrollPaneAuc = new AScrollPane(aucListTable);
 		scrollPaneAuc.setPreferredSize(new Dimension(470, 310));
 		jPAucCnt.add(scrollPaneAuc);
 
 		try {
-			String que = "SELECT  A.AUC_NO, A.PRO_NO, A.USER_NO FROM AUCTION A";
+			String que = "SELECT A.AUC_NO, A.PRO_NO, P.PRO_NAME, U.USER_ID "
+					+ "FROM AUCTION A, USER_T U, PRODUCT_T P "
+					+ "WHERE A.USER_NO = U.USER_NO AND A.PRO_NO = P.PRO_NO";
 
 			conn = dbConn.getConnection();
 			psmt = conn.prepareStatement(que);
 			rs = psmt.executeQuery();
-			System.out.println(que);
 
 			while (rs.next()) {
 				String aucNo = rs.getString(1);
 				String proNo = rs.getString(2);
-				String userNo = rs.getString(3);
+				String proName = rs.getString(3);
+				String userId = rs.getString(4);
 
-				String[] auclist = { aucNo, proNo, userNo };
+				String[] auclist = { aucNo, proNo, proName, userId};
 				modelAuc.addRow(auclist);
 			}
 			rs.close(); psmt.close(); conn.close();
@@ -116,8 +131,70 @@ public class AdminMainPage extends JFrame {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+		// 테이블 생성 1 끝
+		
 
-		// 테이블 붙이기 테스트 끝
+		// 2.회원별 경매내역 - 아이디 검색으로 그 사람의 경매내역을 불러옴
+		JPanel jPAucPrice = new JPanel();
+		jPAucPrice.setBackground(Color.decode("#f45728"));
+		jPAucPrice.setBounds(50, 200, 470, 320);
+		jPAucPrice.setVisible(false);
+		c.add(jPAucPrice);
+		
+		// 테이블 생성 2 - 회원별 경매내역 검색 출력
+		//2. 회원아이디로 검색 -> 프로시저 PRO_USER_AUC_LIST 호출
+		//경매번호, p.그림명, 낙찰가, 경매종료시간
+		DefaultTableModel modelUserAuc = new DefaultTableModel(
+				new String[] { "경매번호", "그림명", "낙찰가격", "경매종료시간"}, 0);
+		ATable userAucTable = new ATable(modelUserAuc);
+		
+		// 각 셀 사이즈 설정
+		userAucTable.getColumn("경매번호").setPreferredWidth(40);
+		userAucTable.getColumn("그림명").setPreferredWidth(200);
+		userAucTable.getColumn("낙찰가격").setPreferredWidth(60);
+		userAucTable.getColumn("경매종료시간").setPreferredWidth(170);
+
+		AScrollPane scrollPaneUserAuc = new AScrollPane(userAucTable);
+		scrollPaneUserAuc.setPreferredSize(new Dimension(470, 310));
+		jPAucPrice.add(scrollPaneUserAuc);
+
+		try {
+			String proc = "pro_user_auc_list";
+			String inUserId = "TESTuser";
+
+			//db연결
+			conn = dbConn.getConnection();
+			//프로시저 호출
+			CallableStatement cs = conn.prepareCall("begin pro_user_auc_list(?); end;");
+			//입력 파라메터
+			cs.setString(1, inUserId);
+			//System.out.println(inUserId);
+			// 출력 파라메터
+			cs.registerOutParameter(1, oracle.jdbc.OracleTypes.CURSOR);
+			// 실행
+			cs.execute();
+			rs = (ResultSet)cs.getObject(1);
+			
+
+			while (rs.next()) {
+				String aucNo = rs.getString(1);
+				String proNo = rs.getString(2);
+				String proName = rs.getString(3);
+				String userId = rs.getString(4);
+
+				String[] userAuclist = { aucNo, proNo, proName, userId};
+				System.out.println(aucNo+ proNo+ proName + userId);
+				
+				modelUserAuc.addRow(userAuclist);
+			}
+			rs.close(); psmt.close(); conn.close();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		// 테이블 생성 2 끝
+		
 		
 		
 		
@@ -126,7 +203,7 @@ public class AdminMainPage extends JFrame {
 		
 
 		// 이벤트 테스트
-		jlAucPrice.addMouseListener(new MouseAdapter() {
+		jbUserAuc.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				jPAucCnt.setVisible(false);
 				jPAucPrice.setVisible(true);
@@ -141,11 +218,11 @@ public class AdminMainPage extends JFrame {
 
 		// 상품관리 버튼+텍스트
 		ALabel jlPdNotice = new ALabel("등록된 상품을 관리합니다");
-		jlPdNotice.setBounds(145, 580, 400, 20);
+		jlPdNotice.setBounds(145, 585, 400, 20);
 		c.add(jlPdNotice);
 
 		AButton btn_listPd = new AButton("상품관리");
-		btn_listPd.setBounds(400, 580, 120, 25);
+		btn_listPd.setBounds(400, 585, 120, 25);
 		c.add(btn_listPd);
 
 		// 상품관리 버튼 이벤트
@@ -188,7 +265,7 @@ public class AdminMainPage extends JFrame {
 		Graphics2D g2 = (Graphics2D) g;
 		Line2D lin1 = new Line2D.Float(20, 125, 580, 125);
 		g2.draw(lin1);
-		Line2D lin2 = new Line2D.Float(20, 580, 580, 580);
+		Line2D lin2 = new Line2D.Float(20, 590, 580, 590);
 		g2.draw(lin2);
 	}
 
