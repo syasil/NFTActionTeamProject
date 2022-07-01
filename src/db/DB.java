79ccbb18e33f334351b7fdaf22592c4d18dea22c
soldesk <soldesk@DESKTOP-G831PP6>
2022-07-01 09:15:37 +0900

    디비

--- src/db/DB.java
+++ src/db/DB.java
@@ -25,4 +25,26 @@ public class DB {
 		
 		return conn;
 	}
+	
+	public static Connection getC() {
+		Connection conn = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String id = "ADMIN";
+			String pw = "pQ5Tf8q=k~$}";
+			String url = "jdbc:oracle:thin:@vft_high?TNS_ADMIN=C:/Work/java/wallet";
+			conn = DriverManager.getConnection(url, id, pw);
+			System.out.println("클라우드 디비 연결 성공");
+			
+		} catch (Exception e) {
+			System.out.println("클라우드 디비 연결 실패");
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	
+	
 }
