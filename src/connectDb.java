f0778d338ba7152ca444899c61bf744f55c0dfd1
syasil <syasil2402@gmail.com>
2022-07-04 14:35:08 +0900

    class 이름 오탈자 수정, 싱글톤화, 정적 메소드 제거

---
+++ src/connectDb.java
@@ -0,0 +1,38 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import Database.DatabaseLinker;
+
+public class connectDb {
+
+	public static connectDb instance = new connectDb();
+	private connectDb() {}
+	public static connectDb getInstance() {
+		return instance;
+	}
+	
+	Connection conn = null;
+	
+	public Connection get() {
+		
+		
+		try {
+
+			String id = "JYY";
+			String pw = "@Tt38003800";
+			String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			conn = DriverManager.getConnection(url, id, pw);
+
+			System.out.println("입력완료");
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+		return conn;
+	}
+
+}
